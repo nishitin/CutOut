f82cbd9520cca343aa59419a21515758bea1e241
nisitin <seiya110307@icloud.com>
2024-10-20 23:08:54 +0900

    feat: 基盤となるモデルの作成

--- server/apps/users/models.py
+++ server/apps/users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)  # メールアドレスをユニークに設定
+    phone_number = models.CharField(max_length=15, blank=True, null=True)  # 電話番号
+    address = models.TextField(blank=True, null=True)  # 住所
+    profile_image = models.ImageField(upload_to='profiles/', blank=True, null=True)  # プロフィール画像
+
+    USERNAME_FIELD = 'email'  # 認証に使用するフィールドをemailに変更
+    REQUIRED_FIELDS = ['username']  # `createsuperuser`時に必須となるフィールド
+
+    def __str__(self):
+        return self.email
